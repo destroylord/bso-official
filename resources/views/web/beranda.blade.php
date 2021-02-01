02b80a7bd2280e9530fdd0cfda8e1aa2443d3c44
destroylord <masapin68@gmail.com>
2021-02-01 12:30:46 +0700

    navbar done

--- resources/views/web/beranda.blade.php
+++ resources/views/web/beranda.blade.php
@@ -1,7 +1,74 @@
 @extends('web.app')
 
 @section('page')
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam soluta aliquid dicta, odio dolores reiciendis ad voluptatum consectetur laborum dignissimos quod, nisi eveniet, sed reprehenderit. Omnis aperiam nesciunt amet sit, nobis, qui quidem ad iure possimus explicabo sunt rerum illo aliquam optio odit quae. Ex, nisi a? Dicta provident dolorem eaque vel nobis quia magnam blanditiis deleniti temporibus ad beatae possimus ab cumque perferendis facilis odit minus officiis natus, qui quaerat neque cum iste labore. Dolorum porro dolorem omnis alias delectus totam mollitia. Debitis, maiores est nobis enim cupiditate non accusamus sint sit ex? Natus molestiae vitae ea dolorum numquam nisi sapiente corrupti, similique sed officiis officia ex et accusantium labore reiciendis veritatis non minima hic odit tempore in magnam explicabo accusamus. Excepturi nisi, alias nihil nam possimus vitae repellendus dolorum ad nostrum fugiat eveniet voluptates, non soluta! Repellendus tempora nemo illum possimus, et enim, quod ex, porro quisquam fugit est minus architecto? Doloribus ipsum totam dolorum saepe voluptate porro optio minima, molestiae quidem dolor odit sequi eligendi accusamus enim similique. Nisi, harum, rerum neque accusantium eligendi rem laudantium nulla consequatur deleniti perspiciatis adipisci, non cum nam eveniet eum enim commodi voluptate molestias facilis dolorem repellendus aut eius? Consequatur, ipsam.
-    </p>
+    <section class="content-banner">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-12">
+                    <div class="banner-con text-center">
+                        <p class="first-title">Selamat Datang di website polije</p>
+                        <p class="banner-des">prototype, design, collabs, and design system all </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="main-content">
+        <section id="cardes">
+            <div class="container">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolores ducimus quod perferendis hic voluptatum mollitia ratione consequatur quibusdam, est quidem officiis asperiores sunt alias pariatur provident blanditiis perspiciatis officia?
+                </p>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolores ducimus quod perferendis hic voluptatum mollitia ratione consequatur quibusdam, est quidem officiis asperiores sunt alias pariatur provident blanditiis perspiciatis officia?
+                </p>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolores ducimus quod perferendis hic voluptatum mollitia ratione consequatur quibusdam, est quidem officiis asperiores sunt alias pariatur provident blanditiis perspiciatis officia?
+                </p>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolores ducimus quod perferendis hic voluptatum mollitia ratione consequatur quibusdam, est quidem officiis asperiores sunt alias pariatur provident blanditiis perspiciatis officia?
+                </p>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolores ducimus quod perferendis hic voluptatum mollitia ratione consequatur quibusdam, est quidem officiis asperiores sunt alias pariatur provident blanditiis perspiciatis officia?
+                </p>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolores ducimus quod perferendis hic voluptatum mollitia ratione consequatur quibusdam, est quidem officiis asperiores sunt alias pariatur provident blanditiis perspiciatis officia?
+                </p>
+            </div>
+        </section>
+    </div>
 @endsection
+@push('scripts')
+    <script>
+        $(function(){
+            var navbar = $('.header-inner');
+            $(window).scroll(function(){
+                if ($(window).scrollTop() <= 50) {
+                    navbar.removeClass('navbar-scroll')
+                }else{
+                    navbar.addClass('navbar-scroll')
+                }
+            })
+        })
+    </script>
+@endpush
--- resources/views/web/includes/header.blade.php
+++ resources/views/web/includes/header.blade.php
@@ -1,3 +1,36 @@
-<h2>
-    Ini header
-</h2>
+<header class="header">
+    <div class="header-inner">
+        <div class="container-fluid px-lg-5">
+            <nav class="navbar navbar-expand-lg my-navbar">
+                <a class="navbar-brand" href="#"> <span class="logo">
+                    <img src="{{ asset('web/img/logo5.png') }}" alt="logo-img" class="img-fluid" style="width: 30p; margin : -3px 0px 0px 0px">Dafrin Web </span>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon">
+                      <i class="fas fa=bars" style="margin: 5px 0px 0px 0px"></i>
+                  </span>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav m-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Kontak</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Struktur Organisasi</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Blog</a>
+                    </li>
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                    <button class="header-btn my-2 my-sm-0" type="submit">Login</button>
+                  </form>
+                </div>
+              </nav>
+        </div>
+    </div>
+</header>
